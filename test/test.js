eb58f52bbc27223c960f8ddfd7b64830f698ebd2
Jefferson Hernandez <jeffersonh44@gmail.com>
2017-03-06 18:58:52 -0500

    working canny with small effects

--- test/test.js
+++ test/test.js
@@ -4,9 +4,9 @@ const cannyEdgeDetector = require('..');
 const Image = require('image-js');
 
 describe('canny-edge-detector test', function () {
-    this.timeout(15000);
+    this.timeout(20000);
     it('Something to test', function () {
-        return Image.load('./img/billboard.jpg').then(function(image) {
+        return Image.load('./img/text.jpg').then(function(image) {
             var grey = cannyEdgeDetector(image);
             grey.save('test.jpg');
         }).catch(function (result) {
