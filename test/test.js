c5b4e5c88a400906fd3a1644ff0905ce0b5a89f9
Jefferson Hernandez <jeffersonh44@gmail.com>
2017-03-07 09:29:48 -0500

    linter changes

--- test/test.js
+++ test/test.js
@@ -5,10 +5,10 @@ const Image = require('image-js');
 
 describe('canny-edge-detector test', function () {
     this.timeout(20000);
-    it('Something to test', function () {
-        return Image.load('./img/billboard.jpg').then(function(image) {
+    it('Main test', function () {
+        return Image.load('./test/img/billboard.jpg').then(function (image) {
             var grey = cannyEdgeDetector(image);
-            grey.save('test.jpg');
+            grey.save('./test/img/result.jpg');
         }).catch(function (result) {
             throw new RangeError(result);
         });
