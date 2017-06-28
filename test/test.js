611136bb58c65b6db511038e4e89ca17e11c651b
Michaël Zasso <targos@protonmail.com>
2017-06-28 10:35:48 +0200

    convert test images to png to avoid CI errors

--- test/test.js
+++ test/test.js
@@ -3,14 +3,14 @@ import cannyEdgeDetector from '../src';
 
 describe('canny-edge-detector test', () => {
     it('Main test', async () => {
-        const promises = [Image.load('./test/img/test1.jpg'), Image.load('./test/img/test2.jpg'), Image.load('./test/img/testres.jpg')];
+        const promises = [Image.load('./test/img/test1.png'), Image.load('./test/img/test2.png'), Image.load('./test/img/testres.png')];
         const images = await Promise.all(promises);
         const params = {
             gaussianBlur: 1.1
         };
         const edges1 = cannyEdgeDetector(images[0].grey(), params);
         const edges2 = cannyEdgeDetector(images[1].grey(), params);
-        const expectedOutput = images[2].grey();
+        const expectedOutput = images[2];
         expectedOutput.colorModel = 'GREY';
 
         expect(expectedOutput.getSimilarity(edges1)).toBeGreaterThan(0.7);
