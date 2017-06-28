948ec56f38c1fd345d3b1dc8ea674a408e927100
Michaël Zasso <targos@protonmail.com>
2017-06-28 10:04:22 +0200

    modernize code

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
-'use strict';
-
-var Image = require('image-js');
-
 const defaultOptions = {
     lowThreshold: 10,
     highThreshold: 30,
-    blur: 1.1
+    gaussianBlur: 1.1
 };
 
+const Gx = [
+    [-1, 0, +1],
+    [-2, 0, +2],
+    [-1, 0, +1]
+];
 
-/**
- * Find edges in an image using the Canny algorithm
- * @param {Image} image : a greyscale image
- * @param {object} options
- * @param {number} [options.lowThreshold=10] : first threshold for the hysteresis procedure.
- * @param {number} [options.highThreshold=30] : second threshold for the hysteresis procedure.
- * @param {number} [options.blur=1.1] : sigma parameter for gaussian filter step.
- * @param {number} [options.brightness] : values assigned to each edge pixel on the result image.
- * @return {Image} : an image with the edges at options.brightness value.
- */
-function cannyEdgeDetector(image, options) {
-    image.checkProcessable("Canny edge detector", {
+const Gy = [
+    [-1, -2, -1],
+    [0, 0, 0],
+    [+1, +2, +1]
+];
+
+const convOptions = {
+    bitDepth: 32,
+    mode: 'periodic'
+};
+
+export default function cannyEdgeDetector(image, options) {
+    image.checkProcessable('Canny edge detector', {
         bitDepth: 8,
         channels: 1,
         components: 1
@@ -28,47 +30,35 @@ function cannyEdgeDetector(image, options) {
 
     options = Object.assign({}, defaultOptions, options);
 
-    var width = image.width, height = image.height;
-    var brightness = image.maxValue;
+    const width = image.width;
+    const height = image.height;
+    const brightness = image.maxValue;
 
-    var Gx = [
-        [-1, 0, +1],
-        [-2, 0, +2],
-        [-1, 0, +1]
-    ];
-
-    var Gy = [
-        [-1, -2, -1],
-        [0, 0, 0],
-        [+1, +2, +1]
-    ];
-
-    var gfOptions = {
-        sigma: options.blur,
+    const gfOptions = {
+        sigma: options.gaussianBlur,
         radius: 3
     };
 
-    var gf = image.gaussianFilter(gfOptions);
+    const gf = image.gaussianFilter(gfOptions);
 
-    var convOptions = {
-        bitDepth: 32,
-        mode: 'periodic'
-    };
+    const gradientX = gf.convolution(Gy, convOptions);
+    const gradientY = gf.convolution(Gx, convOptions);
 
-    var gradientX = gf.convolution(Gy, convOptions);
-    var gradientY = gf.convolution(Gx, convOptions);
+    const G = gradientY.hypotenuse(gradientX);
 
-    var G = gradientY.hypotenuse(gradientX);
+    const Image = image.constructor;
 
-    var nms = new Image(width, height, {
+    const nms = new Image(width, height, {
         kind: 'GREY',
         bitDepth: 32
     });
-    var edges = new Image(width, height, {
+
+    const edges = new Image(width, height, {
         kind: 'GREY',
         bitDepth: 32
     });
-    var finalImage = new Image(width, height, {
+
+    const finalImage = new Image(width, height, {
         kind: 'GREY'
     });
 
@@ -77,13 +67,12 @@ function cannyEdgeDetector(image, options) {
         for (var j = 1; j < height - 1; j++) {
 
             var dir = (Math.round(Math.atan2(gradientY.getValueXY(i, j, 0), gradientX.getValueXY(i, j, 0)) * (5.0 / Math.PI)) + 5) % 5;
-            //dir %= 4;
 
             if (
                 !((dir === 0 && (G.getValueXY(i, j, 0) <= G.getValueXY(i, j - 1, 0) || G.getValueXY(i, j, 0) <= G.getValueXY(i, j + 1, 0)))
-                || (dir === 1 && (G.getValueXY(i, j, 0) <= G.getValueXY(i - 1, j + 1, 0) || G.getValueXY(i, j, 0) <= G.getValueXY(i + 1, j - 1, 0)))
-                || (dir === 2 && (G.getValueXY(i, j, 0) <= G.getValueXY(i - 1, j, 0) || G.getValueXY(i, j, 0) <= G.getValueXY(i + 1, j, 0)))
-                || (dir === 3 && (G.getValueXY(i, j, 0) <= G.getValueXY(i - 1, j - 1, 0) || G.getValueXY(i, j, 0) <= G.getValueXY(i + 1, j + 1, 0))))
+                    || (dir === 1 && (G.getValueXY(i, j, 0) <= G.getValueXY(i - 1, j + 1, 0) || G.getValueXY(i, j, 0) <= G.getValueXY(i + 1, j - 1, 0)))
+                    || (dir === 2 && (G.getValueXY(i, j, 0) <= G.getValueXY(i - 1, j, 0) || G.getValueXY(i, j, 0) <= G.getValueXY(i + 1, j, 0)))
+                    || (dir === 3 && (G.getValueXY(i, j, 0) <= G.getValueXY(i - 1, j - 1, 0) || G.getValueXY(i, j, 0) <= G.getValueXY(i + 1, j + 1, 0))))
             ) {
                 nms.setValueXY(i, j, 0, G.getValueXY(i, j, 0));
             }
@@ -104,7 +93,6 @@ function cannyEdgeDetector(image, options) {
         edges.data[i] = currentEdge;
     }
 
-
     // Hysteresis: first pass
     var currentPixels = [];
     for (i = 1; i < width - 1; ++i) {
@@ -113,13 +101,11 @@ function cannyEdgeDetector(image, options) {
                 continue;
             }
 
-            var end = false;
             outer: for (var k = i - 1; k < i + 2; ++k) {
                 for (var l = j - 1; l < j + 2; ++l) {
                     if (edges.getValueXY(k, l, 0) === 2) {
                         currentPixels.push([i, j]);
                         finalImage.setValueXY(i, j, 0, brightness);
-                        end = true;
                         break outer;
                     }
                 }
@@ -149,7 +135,4 @@ function cannyEdgeDetector(image, options) {
     }
 
     return finalImage;
-
 }
-
-module.exports = cannyEdgeDetector;
--- test/test.js
+++ test/test.js
@@ -1,25 +1,19 @@
-'use strict';
+import Image from 'image-js';
+import cannyEdgeDetector from '../src';
 
-const cannyEdgeDetector = require('..');
-const Image = require('image-js');
+describe('canny-edge-detector test', () => {
+    it('Main test', async () => {
+        const promises = [Image.load('./test/img/test1.jpg'), Image.load('./test/img/test2.jpg'), Image.load('./test/img/testres.jpg')];
+        const images = await Promise.all(promises);
+        const params = {
+            gaussianBlur: 1.1
+        };
+        const edges1 = cannyEdgeDetector(images[0].grey(), params);
+        const edges2 = cannyEdgeDetector(images[1].grey(), params);
+        const expectedOutput = images[2].grey();
+        expectedOutput.colorModel = 'GREY';
 
-describe('canny-edge-detector test', function () {
-    this.timeout(20000);
-    it('Main test', function () {
-        var promises = [Image.load('./test/img/test1.jpg'), Image.load('./test/img/test2.jpg'), Image.load('./test/img/testres.jpg')];
-
-        return Promise.all(promises).then(function (images) {
-            var params = {
-                blur: 1.1
-            };
-            var edges1 = cannyEdgeDetector(images[0].grey(), params), edges2 = cannyEdgeDetector(images[1].grey(), params);
-            var expectedOutput = images[2].grey();
-            expectedOutput.colorModel = 'GREY';
-
-            expectedOutput.getSimilarity(edges1).should.be.above(0.7);
-            expectedOutput.getSimilarity(edges2).should.be.above(0.7);
-        }).catch(function (result) {
-            throw new RangeError(result);
-        });
+        expect(expectedOutput.getSimilarity(edges1)).toBeGreaterThan(0.7);
+        expect(expectedOutput.getSimilarity(edges2)).toBeGreaterThan(0.7);
     });
 });
